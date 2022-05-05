9071b96a88ad3eb8ec74704763952f2a8708ee08
Lucas <lucasmartin13@yahoo.de>
2022-05-05 20:49:35 +0200

    added first Recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Recipe from "./Recipe";
+
 function App() {
   return (
     <div className="App">
-      <h1>REZ</h1>
+      <Recipe />
     </div>
   );
 }
