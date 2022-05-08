91b6c5ca01df7612d4f7b53c9b631c03a92745ff
Lucas <lucasmartin13@yahoo.de>
2022-05-08 14:25:06 +0200

    added form list for ingredients and prep-steps

--- src/RecipeForm.tsx
+++ src/RecipeForm.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {
   Box,
-  Button,
+  Button, Grid,
   InputAdornment,
   List,
   ListItem,
@@ -27,6 +27,7 @@ const StyledRating = styled(Rating)({
 
 export default function RecipeForm() {
   const [ingredients, setIngredients] = useState([{name:"",amount:"",unit:""}]);
+  const [preparations,setPreparations] = useState<string[]>([""]);
 
   const levelMarks = [
     {
@@ -43,47 +44,85 @@ export default function RecipeForm() {
     }
   ];
 
-
-  function renderIngredients() {
+  function renderPreparation() {
     return(
-        ingredients.map((ingredient, index:number)=>(
-          <ListItem key={index}>
-            <TextField
-              type="text"
-              label="Name"
-              variant="standard"
-              name="name"
-              value={ingredient.name}
-              onChange={ (e) => handleInputChange(e,index)}
-            />
-            <TextField
-              name="amount"
-              type="text"
-              label="Menge"
-              variant="standard"
-              inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-              onChange={ (e) => handleInputChange(e,index)}
-              value={ingredient.amount}
-            />
-            <TextField
-              name="unit"
-              type="text"
-              label="Einheit"
-              variant="standard"
-              onChange={ (e) => handleInputChange(e,index)}
-              value={ingredient.unit}
-            />
-            <Button
-              onClick={()=>handleRemoveClick(index)}
-            >
-              <DeleteIcon/>
-            </Button>
-          </ListItem>
-        ))
+      preparations.map((step, index:number)=>(
+        <ListItem key={index}>
+          <TextField
+            type="text"
+            label={"Schritt "+(index+1)}
+            variant="standard"
+            name="name"
+            value={step}
+            onChange={ (e) => handlePrepInputChange(e,index)}
+          />
+          <Button
+            onClick={()=>handlePrepRemoveClick(index)}
+          >
+            <DeleteIcon/>
+          </Button>
+        </ListItem>
+      ))
     );
   }
 
-  const handleInputChange = (e:any, index:any) => {
+  const handlePrepInputChange = (e:any, index:any) => {
+    const { value } = e.target;
+    const list = [...preparations];
+    list[index] = value;
+    setPreparations(list);
+  };
+
+  const handlePrepRemoveClick = (index:number) => {
+    const list = [...preparations];
+    list.splice(index, 1);
+    setPreparations(list);
+  };
+
+  const handlePrepAddClick = () => {
+    setPreparations([...preparations, ""]);
+  }
+
+  function renderIngredients() {
+    return(
+      ingredients.map((ingredient, index:number)=>(
+        <ListItem key={index}>
+          <TextField
+            type="text"
+            label="Name"
+            variant="standard"
+            name="name"
+            value={ingredient.name}
+            onChange={ (e) => handleIngredientInputChange(e,index)}
+          />
+          <TextField
+            name="amount"
+            type="text"
+            label="Menge"
+            variant="standard"
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={ (e) => handleIngredientInputChange(e,index)}
+            value={ingredient.amount}
+          />
+          <TextField
+            name="unit"
+            type="text"
+            label="Einheit"
+            variant="standard"
+            onChange={ (e) => handleIngredientInputChange(e,index)}
+            value={ingredient.unit}
+          />
+          <Button
+            onClick={()=>handleIngredientRemoveClick(index)}
+          >
+            <DeleteIcon/>
+          </Button>
+        </ListItem>
+      ))
+    );
+  }
+
+  const handleIngredientInputChange = (e:any, index:any) => {
     const { name, value } = e.target;
     const list = [...ingredients];
     switch (name){
@@ -100,13 +139,13 @@ export default function RecipeForm() {
     setIngredients(list);
   };
 
-  const handleRemoveClick = (index:number) => {
+  const handleIngredientRemoveClick = (index:number) => {
     const list = [...ingredients];
     list.splice(index, 1);
     setIngredients(list);
   };
 
-  function handleAddClick() {
+  const handleIngredientAddClick = () => {
     setIngredients([...ingredients, { name: "", amount: "", unit: "" }]);
   }
 
@@ -124,60 +163,78 @@ export default function RecipeForm() {
         noValidate
         autoComplete="off"
       >
-        <TextField
-          id="name"
-          label="Name"
-          variant="standard" />
-        <TextField
-          id="time"
-          label="Zeit"
-          type="text"
-          variant="standard"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">min</InputAdornment>,
-            inputProps: {
-              min: 0,
-              inputMode: 'numeric',
-              pattern: "[0-9]*"
-            },
-          }}
-        />
-        <Slider
-          aria-label="level"
-          defaultValue={0}
-          step={1}
-          marks={levelMarks}
-          min={0}
-          max={2}
-          sx={{width:200}}
-          track={false}
-        />
-        <Box >
-          <Typography component="legend">Bewertung</Typography>
-          <StyledRating
-            name="rating"
-            defaultValue={2}
-            precision={1}
-            icon={<LocalDiningIcon fontSize="inherit" />}
-            emptyIcon={<LocalDiningIcon fontSize="inherit" />}
-            sx={{magin:'auto'}}
+        <Grid
+          container
+          direction="column"
+        >
+          <TextField
+            id="name"
+            label="Name"
+            variant="standard" />
+          <TextField
+            id="time"
+            label="Zeit"
+            type="text"
+            variant="standard"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">min</InputAdornment>,
+              inputProps: {
+                min: 0,
+                inputMode: 'numeric',
+                pattern: "[0-9]*"
+              },
+            }}
           />
-        </Box>
-        <Box>
-          <List>
-            <ListSubheader>Zutaten</ListSubheader>
-            {renderIngredients()}
-            <ListItem>
-              <Button
-                sx={{margin:"auto"}}
-                onClick={handleAddClick}
-              >
-                <AddCircleIcon/>
-              </Button>
-            </ListItem>
-          </List>
-        </Box>
-
+          <Slider
+            aria-label="level"
+            defaultValue={0}
+            step={1}
+            marks={levelMarks}
+            min={0}
+            max={2}
+            sx={{width:200}}
+            track={false}
+          />
+          <Box >
+            <Typography component="legend">Bewertung</Typography>
+            <StyledRating
+              name="rating"
+              defaultValue={2}
+              precision={1}
+              icon={<LocalDiningIcon fontSize="inherit" />}
+              emptyIcon={<LocalDiningIcon fontSize="inherit" />}
+              sx={{magin:'auto'}}
+            />
+          </Box>
+          <Box>
+            <List>
+              <ListSubheader>Zutaten</ListSubheader>
+              {renderIngredients()}
+              <ListItem>
+                <Button
+                  sx={{margin:"auto"}}
+                  onClick={handleIngredientAddClick}
+                >
+                  <AddCircleIcon/>
+                </Button>
+              </ListItem>
+            </List>
+          </Box>
+          <Box>
+            <List>
+              <ListSubheader>Zubereitung</ListSubheader>
+              {renderPreparation()}
+              <ListItem>
+                <Button
+                  sx={{margin:"auto"}}
+                  onClick={handlePrepAddClick}
+                >
+                  <AddCircleIcon/>
+                </Button>
+              </ListItem>
+            </List>
+          </Box>
+        </Grid>
       </Box>
     </>
   );
